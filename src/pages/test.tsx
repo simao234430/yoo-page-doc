6baa12584be458cacb90044ba32866a88b9fbe63
simao234430 <simao234430@gmail.com>
2023-08-17 10:56:46 +0800

    fix th

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,11 +1,11 @@
-import Tag from '@/yoo-components/packages/Tag';
-import { IconCopy,IconLarkColor,IconDownCircle } from '@arco-design/web-react/icon';
+// import    Button   from '../../yoo-components/packages/Button'
+import  {Button}    from "@simao234430/yoo-components"
  
 export default function Test( ) {
     return (
         <div >
  
- <Tag color='red'>我是标签</Tag>
+ <Button style={{color:'blue'}}>Button</Button>
         {/* <IconCopy className='test'   ></IconCopy>
         <IconDownCircle  fill='white'  stroke='black' />
         <IconLarkColor />
